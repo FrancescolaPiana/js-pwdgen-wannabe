c81e6eb911e33a05affa59ea1d0233b5b0f820de
Francescolapiana <Lushika1219@gmail.com>
2022-10-06 14:52:57 +0200

    hotwell

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let nome;
+let cognome;
+let colore;
+let eta;
+let html;
+let password ;
+let nnt;
+
+nome = prompt('Scrivi il tuo Nome?');
+cognome = prompt('Scrivi il tuo Cognome');
+colore = prompt('Scrivi il tuo colore preferito');
+eta = prompt('Scrivi la tua età');
+
+password = nome + cognome + colore + eta;
+
+console.log(password);
+
+document.getElementById('psw') = password;
+
+// ntt =  ${password};
+
+
+// elm = document.getElementById('password');
+// elm = ntt;
